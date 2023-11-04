00ed562348b1374c85d1bd54489126214ab74a21
Alexandre Peinado <apeinadollopis@gmail.com>
2023-11-04 18:19:57 +0100

    Initial Commit

---
+++ SuperHeroesApp/SuperHeroesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroesAppApp.swift
+//  SuperHeroesApp
+//
+//  Created by Alex Peinado Llopis on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
